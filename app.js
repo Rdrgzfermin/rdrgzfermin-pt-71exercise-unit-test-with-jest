29383f40259e6815bbec57a68ef2830b7e2a606b
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-09-18 22:43:55 +0000

    first save

---
+++ app.js
@@ -0,0 +1,10 @@
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+const fromEuroToDollar = () => {};
+const fromDollarToYen = () => {};
+const fromYenToPound = () => {};
+
